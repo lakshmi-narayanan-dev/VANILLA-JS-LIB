98a1f7ad787137f25a9f8637ddb476910a04096b
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-23 17:59:34 +0530

    Add files via upload

---
+++ Intro + CDn.js
@@ -0,0 +1,1 @@
+https://releases.jquery.com/ - CDN
---
+++ JS-Lib.js
@@ -0,0 +1,494 @@
+-------------------------------------------------------------
+Challenges
+-------------------------------------------------------------
+https://www.odoo.com/jobs/challenge/next?wrong=oups 
+
+
+-------------------------------------------------------------
+Vs Code Functionalities
+-------------------------------------------------------------
+File Explorer + Code
+—------------------------
+Alt + Up / down arrow - Move code up & down with cut copy paste
+Ctrl + Shift + B - Show / hide toggle button	
+Ctrl + Shift + X - Extensions
+Ctrl + Shift + E - FIle manager
+Alt + L + O - Live Server
+Ctrl + K + T - Change Theme
+
+
+-----------------------
+Extensions
+—---------------------
+Package JSON Upgrade  - Use to display all package upgraded versions 
+
+
+
+—------------------------
+Run and Debug 
+—------------------------
+
+—------------------------
+Git + SSH
+—------------------------
+https://pipelinedriven.org/	
+
+
+
+
+------------------------------------------------------------------------
+Front End - Html Browser Css Sass Scss BS TW CS PHP git UX
+-----------------------------------------------------------------------
+Cheat sheet
+—------------------------
+https://overapi.com/
+https://andreasbm.github.io/web-skills/  - All link in one page
+https://www.w3docs.com/ - W3
+https://www.100daysofcode.com/ 
+https://devdocs.io/ 
+https://www.freecodecamp.org/news/career-mistakes-to-avoid-as-a-dev/ 
+----
+UX
+----
+https://app.diagrams.net/?src=about 
+https://autodraw.com/ 
+
+
+
+
+—------------------------
+PHP
+—------------------------
+https://www.freecodecamp.org/news/php-vs-javascript-which-technology-will-suit-your-business-better/ 
+
+
+
+------------------------------
+Css properties + Codepen
+------------------------------
+Animation -Card Hover - https://codepen.io/pleasedonotdisturb/pen/oNQLVXB 
+Accordion Css - https://codepen.io/abergin/pen/BaKVWd 
+Dropdown Css - https://codemyui.com/tag/drop-down/page/2/ 
+https://codemyui.com/all-tags/ - All Css Tags
+
+------------------
+Css Animation 
+------------------
+https://www.fcinq.com/fr/ 
+https://codepen.io/halvo/pen/BWQxWm 
+ https://www.dofactory.com/css/animation 
+
+
+--------------------------------
+Video + YT + Sound website
+--------------------------------
+video section 
+https://intothetrenches.1917.movie/  video load after sometimes
+https://stackoverflow.com/questions/52887444/hide-more-videos-within-youtube-iframe-when-stop-video 
+SRT - Subtitle file format for video contents 
+https://themezinho.net/agensy/?storefront=envato-elements 
+
+--------------------------------
+Images + Favicons
+--------------------------------
+https://fffuel.co/llline/ 
+https://svgontheweb.com/ 
+https://favicon.io/favicon-converter/ 
+https://stackoverflow.com/questions/19029342/favicons-best-practices 
+https://css.gg/ + https://github.com/astrit/css.gg/tree/master [Css.gg]
+FontAwesome + Lineicons
+https://iconmonstr.com/
+https://www.streamlinehq.com/
+https://getloaf.io/   - Animated SVG
+https://webkul.github.io/vivid/cheatsheet.html
+https://www.youtube.com/watch?v=lBK_kh4iCAQ + https://www.youtube.com/watch?v=-u7pzg3GcFU - Splash Screen Bg
+https://codepen.io/Rueb/pen/aGOmor - Hover Image tooltip
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------
+Javascript + Libraries + Node Js + Website + Code + Links
+---------------------------------------------------------------------
+Libraries + Its uses
+—----------------------
+Js Eg = > https://www.freecodecamp.org/news/javascript-example/ 
+
+
+https://www.geeksforgeeks.org/top-10-javascript-carousal-libraries-used-in-2023/  - 10 Libraries for JS
+https://kinsta.com/blog/javascript-libraries/ - 40 Libraries
+https://driverjs.com/ - Product Tours
+https://glidejs.com/docs/ - Lightweight Carousel
+https://swiperjs.com/ - Modern Carousel
+https://kenwheeler.github.io/slick/ - Slick 
+https://splidejs.com/ - Typescript carousel 
+Owl Carousel [ https://on3-step.com/tf/stock/html/index.html ]
+https://semaphoreci.com/blog/locomotive-scroll-react-scroll-parallax + https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/ = locomotive scroll
+ https://www.sliderrevolution.com/resources/css-animations-on-scroll/ 
+https://morioh.com/a/d8f7869ad522/how-to-create-a-parallax-scrolling-effect 
+https://highrise.digital/blog/animating-blocks-on-scroll-with-intersection-observer/ 
+https://splitting.js.org/ - text Reveal JS
+
+
+https://www.freecodecamp.org/news/how-to-write-more-efficient-javascript-conditions/ 
+
+--------------
+Functions
+--------------
+https://www.freecodecamp.org/news/the-difference-between-arrow-functions-and-normal-functions/ 
+
+----------------------
+AOS + Scroll Reveal
+----------------------
+https://github.com/michalsnik/aos/issues/159 
+https://www.sitepoint.com/cool-scroll-animations-made-easy-aos-library/  + https://michalsnik.github.io/aos/ 
+https://scrollrevealjs.org/ 
+https://michalsnik.github.io/aos/ 
+
+---------------
+jQuery
+---------------
+https://www.sitepoint.com/jquery-refresh-page-browser-resize/ 
+https://codepen.io/intotheprogram/pen/rNNXOPq  - Text Fade
+https://codepen.io/glfoster/pen/JodpMw 
+https://codemyui.com/slides-text-splitting-middle/ - Click 2 split screen
+
+---------------
+GSAP
+---------------
+https://codepen.io/ilimitadostudio/pen/qBQyeeN 
+https://codepen.io/freedommayer/pen/VwVjjyX - text reveal on scroll
+https://codepen.io/rachsmith/post/how-to-move-elements-on-scroll-in-a-way-that-doesn-t-suck-too-bad 
+https://codepen.io/patrickcole/pen/vKZOjb 
+https://gsap.com/docs/v3/Installation/ + https://scrollmagic.io/examples/expert/image_sequence.html + https://codepen.io/Juxtopposed/pen/NWExxja 
+https://gsap.com/docs/v3/Installation/ + https://gsap.com/community/forums/topic/23374-locomotive-scroll-and-gsap/
+
+
+
+
+------------------------------------
+Dark Night Switch with preload
+-------------------------------------
+https://www.stinkstudios.com/about 
+
+-------------------------------------
+Progress Bar Js
+-------------------------------------
+https://codepen.io/AllThingsSmitty/pen/gvpNWL 
+
+---------------------
+Reveal Screen Js
+---------------------
+https://codepen.io/AureleJ/pen/oNQvbed 
+----------------
+Top Button Js
+----------------
+https://css-tricks.com/how-to-make-an-unobtrusive-scroll-to-top-button/ 
+
+
+
+------------------------------------
+Async Defer
+------------------------------------
+https://javascript.info/script-async-defer 
+Defer Async Speed 
+Speed 1 Js  
+https://javascript.info/script-async-defer
+
+
+—-----------------------------
+naraine-developer.website
+—-----------------------------
+1) Preloader
+Preloader with Js (settimeout + Dom loaded) + Reveal screens 
+Preloader with jQuery (settimeout + Dom loaded ) + Reveal screens
+Preloader with ReactJs
+https://rrdevs.net/demos/html/consulter/services.html - Reveal Screen
+https://bulkit.cssninja.io/kit1-landing-3.html 
+
+2) Navbar
+Get 5 links here - replicate here.
+
+https://saucelabs.com/ 
+
+https://nomadictribe.com/#regions 
+
+https://nakashimawoodworkers.com/ 
+
+https://www.akzonobel.com/en# 
+
+https://www.mapbox.com/matrix-api 
+
+Hide show scroll navbar - Scroll up and down navbar hide
+
+Navbar 6. 
+
+Js + jQuery + Bs + Tw + Css (spend more time to under navbar)
+
+https://kopi.dev/tailwind/navbar-responsive-mega-menu-using-tailwind-ui/ - TW
+
+https://codepen.io/ingvoo/pen/ZVOqbP 
+
+https://codepen.io/prvnbist/pen/GQMPZq 
+
+
+
+
+
+5)Js Library
+
+
+
+
+Blogs Page
+Medium Blogs 
+Blog structure 
+Subscribing Details Js + PHP
+
+CTA section to hire me
+Download my Resume with Decent css section 
+
+Buttons all type of hovering effect + Border light gradient + responsiveness + hover arrow / svg translate
+
+Dark Light mode
+https://www.stinkstudios.com/about 
+https://html.themexriver.com/chalter/chalter/index.html 
+
+Timer+Trigger count
+jQuery + Js + Js library
+https://thehimanshuagrawal.com/300-400-appointments/ 
+https://elfsight.com/number-counter-widget/examples/ 
+https://nicepage.com/c/counter-website-design 
+
+https://scrollrevealjs.org/guide/installation.html 
+https://scrollmagic.io/examples/advanced/section_slides_manual.html 
+https://kenwheeler.github.io/slick/ 
+
+
+https://stackoverflow.com/questions/39159236/my-swiper-with-swiper-js-stop-autoplay-after-slide-it-with-finger-why 
+https://stackoverflow.com/questions/64100644/swiper-slider-how-to-disable-swiping-and-mouse-wheel-scrolling-during-the-t 
+
+https://www.javatpoint.com/jquery-bind 
+
+https://glidejs.com/docs/ 
+https://learn.jquery.com/using-jquery-core/document-ready/ 
+
+
+https://search.brave.com/search?q=what+is+ajax+jquery&source=desktop 
+https://www.cloudflare.com/en-gb/ 
+
+ScrollTrigger + Top Button
+https://www.huddle.com.co/#packages 
+https://codepen.io/dudleystorey/pen/mdQEXK 
+
+
+Text
+https://codepen.io/keukenrolletje/pen/ojvErX 
+https://codepen.io/alvaromontoro/pen/RmRjvg 
+https://codepen.io/N-Cristina/pen/OJMJxgo 
+https://codepen.io/atled/pen/ajQZvg 
+https://www.linuxfordevices.com/tutorials/ubuntu/install-tp-link-wifi-adapter-driver-in-linux 
+
+Modal + Popup
+https://codepen.io/serkan-cetinkaya/pen/EKGNQe 
+
+
+Hobbies + personal interest 
+Movies liked - make it like Netflix or any streaming site
+
+Orator Favorite
+
+Series liked
+
+Character liked
+
+Sports
+Gallery switch tab - Bs Tw Css
+
+Boxing
+Cricket 
+Gym 
+Weight lifting 
+
+Contact Page
+Php , Form , details validation 
+Nodejs + php + phpmyadmin + SQL
+Js validation using nodejs
+https://stackoverflow.com/questions/31602954/how-to-redirect-folders-browsing-to-error-404-page 
+
+
+Rss Feed News 
+RSS 
+
+
+
+Tw + animation
+https://markustripp.medium.com/animations-with-tailwind-css-c47534e57a18 
+
+Timeline scroll
+https://99designs.com/profiles/BaicheRamy/designs/2054476 
+
+Landing/Sales Pages (max15)
+https://www.masterclass.com/classes/aaron-sorkin-teaches-screenwriting 
+https://www.ivang-design.com/aoide/?storefront=envato-elements 
+https://entrepreneurstowealthprenurschallenge.com/etw5/join-workshop-news/ (Bommer Ideas)
+https://max-themes.net/demos/grandconference/?storefront=envato-elements# 
+https://www.feedmusic.com/ 
+https://www.airbnb.co.in/host/homes 
+https://html.themexriver.com/avarton/contact.html# 
+https://www.simplilearn.com/pgp-devops-certification-training-course 
+https://mixdesign.club/themeforest/helio/index-agency-triangles-slider.html 
+https://99designs.com/blog/creative-inspiration/landing-page-design-inspiration/ (pick anyone)
+https://lifeplugin.com/events/summit/ 
+https://www.atlassian.com/company/people 
+https://matchstic.com/about 
+https://www.godaddy.com/en-in/contact-us 
+https://themenectar.com/salient/blog-magazine/ 
+https://www.themesindustry.com/html/reone/demos/?storefront=envato-elements (choose any 2)
+https://amcuae.com/work/ 
+https://www.convertflow.com/call-to-action/growth-design-long-form-landing-page 
+https://organizations.hehttps://organizations.headspace.com/adspace.com/  [2nd section hover, 3 section logo slide, 1 FAQ active section, Counters, Footer Design]
+https://www.semplice.com/
+https://preview.themeforest.net/item/insurez-insurance-company-wordpress-theme/full_screen_preview/46328557?_ga=2.1624433.1785284329.1687860369-127091116.1677131736 
+
+
+
+Website to replicate (max15)
+https://anydesk.com/en  
+https://deepmind.google/technologies/gemini/#introduction 
+https://www.mozilla.org/en-US/ 
+https://99designs.com/profiles/BaicheRamy/designs/2054476 
+https://www.convertflow.com/call-to-action/case-study-buddy-landing-page 
+https://www.mindvalley.com/ 
+https://digital-cover.com/ 
+https://www.saurabhkaushik.com/
+https://www.themesindustry.com/html/reone/index11.html 
+https://themezinho.net/agensy/?storefront=envato-elements 
+http://preview.themeforest.net/item/harington-creative-portfolio-theme/full_screen_preview/42929633?_ga=2.249999622.1692960803.1680324453-1772057299.1677130958 
+https://evernote.com/ 
+https://kalanidhithemes.com/live-preview/landing-page/apper/all-demo/03-app-landing-page-wave-animation/index.html# 
+https://preview.themeforest.net/item/dax-apartment-complex-landing-page-html-template/full_screen_preview/45858871?_ga=2.176902757.1515702210.1688116167-1424666922.1688116167&_gac=1.189288153.1688116167.CjwKCAjw-vmkBhBMEiwAlrMeF945nv6zdpcA5WYiioV9i6bZ0dhQshIFjh5W-s81RAP_72SjUXXZMhoC7noQAvD_BwE 
+https://preview.colorlib.com/theme/glint/ 
+https://themenectar.com/salient/nonprofit/#sidewidgetarea 
+https://mastermind.com/# 
+https://themezinho.net/tourog/?storefront=envato-elements 
+https://preview.themeforest.net/item/consen-finance-and-consulting-template/full_screen_preview/42234252?_ga=2.153384376.1610593292.1691734802-1313767095.1682000628 
+https://www.themenate.net/eastern/index-default.html 
+https://wise.com/us/ 
+https://harnishdesign.net/demo/react/callum/demo/?theme=Full%20Screen%20Menu&intro=default 
+https://www.flyhyer.com/ 
+https://www.digitalmarmalade.co.uk/ 
+https://plasm.it/
+https://www.ruby.com/ [Navbar + Font]
+https://www.engagebay.com/ [Image slide section , email subscribe , Icons, Top Button , few scroll ad popup ]
+https://www.wix.com/website-template/view/html/1896?siteId=c54249f0-9878-4464-9dc0-5cc938a097e4&metaSiteId=2f5a38ee-e1bf-4a2d-a169-8e9670dd4cb0&originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Flanding-pages - [Coming Soon Page pakka]
+https://www.brasshands.com/ [Entire page - selection , cursor , slides , image slide on view]
+https://preview.colorlib.com/theme/eclipse/# [Course slide , navbar color change with scroll]
+https://preview.colorlib.com/theme/glint/ [Scroll Bg image move, Gallery section]
+https://www.hotjar.com/ [Entire page replicate]
+https://demos.codexworld.com/code-snippet-page-scroll-progress-bar-with-css-javascript/ [Progress Bar]
+https://softcodeon.com/tutorials/how-to-add-preloader-in-html-page.htm [Creative Preloader with reveal screen]
+https://www.freecodecamp.org/news/how-to-make-clickable-svg-map-html-css/ , https://bgjar.com/ , https://haikei.app/ , https://www.freecodecamp.org/news/svg-tutorial-learn-to-code-images/[Svg Exploration]
+https://codewithsadee.github.io/jack-portfolio/ , https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode - [Dark/Light Theme]
+https://www.tailwindawesome.com/resources/jane/demo [Replicate the page]
+https://www.devwares.com/blog/create-bootstrap-charts-using-bootstrap5/ [BS Charts with JS]
+https://www.freecodecamp.org/news/how-to-build-a-responsive-navigation-bar-with-dropdown-menu-using-javascript/ [Navbar Step By Step Explaination]
+https://www.section.io/engineering-education/creating-a-responsive-navigation-bar-using-tailwind-css-and-javascript/ [TW navbar]
+https://www.w3schools.com/howto/howto_css_parallax.asp [Parallex Effect Easy Css]
+https://events.paloaltonetworks.com/ [Navbar + Slider 1]
+https://saucelabs.com/ [Slider text animation]
+https://www.elegantthemes.com/gallery/divi/ [On scroll section show , YT Video play effect, ]
+https://d3js.org/ - [Section]
+https://codepen.io/DevillersJerome/pen/EXYBZZ - [text underline or circle]
+https://dianadanieli.com/ [Preloader page]
+https://www.100tasks.com/ [1st Section]
+https://codepen.io/about [Font + Replicate the page]
+https://codepen.io/rachsmith/post/how-to-move-elements-on-scroll-in-a-way-that-doesn-t-suck-too-bad [Move object while scrolling]
+https://damian.drygiel.com/ 
+
+https://resumeworded.com/web-developer-resume-examples 
+
+---------------------------------------
+MAANG
+---------------------------------------
+G - https://www.google.com/about/careers/applications/jobs/results/ 
+N - https://jobs.netflix.com/locations/mumbai-india 
+A - https://www.apple.com/careers/in/work-at-apple.html 
+A -AmazonJ 
+
+---------------------------------------
+Links-Code-Features
+---------------------------------------
+	
+Favicons
+https://developers.google.com/search/docs/appearance/favicon-in-search#:~:text=A%20favicon%20isn't%20guaranteed,can%20have%20two%20different%20favicons. 
+https://www.seoptimer.com/blog/what-is-a-favicon/ 
+
+
+https://www.google.com/sheets/about/ - UX
+Navbar > Logo > Bg color overflow container bottom by 10px looks like a tag.
+https://themeplace.pro/article/?g=2466 
+
+
+Carousel
+https://sendpulse.com/blog/website-carousel-design#:~:text=a%20single%20platform.-,What%20is%20the%20difference%20between%20carousels%20and%20sliders%3F,see%20multiple%20slides%20at%20once 
+
+
+
+Icons
+https://www.tutorialrepublic.com/font-awesome-icons-classes.php 
+https://www.w3schools.com/icons/fontawesome_icons_webapp.asp 
+https://materializecss.com/icons.html 
+https://icons.getbootstrap.com/ 
+https://fonts.google.com/icons 
+https://ionic.io/ionicons 
+
+Nodejs - Mongo - Form - Login
+https://stackoverflow.com/questions/50448214/inserting-form-data-into-mysql-database-when-using-nodejs-express 
+https://www.simplilearn.com/tutorials/php-tutorial/php-login-form 
+https://dzone.com/articles/ceate-a-login-system-using-html-php-and-mysql 
+
+Search using JS
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter 
+https://blog.openreplay.com/implementing-live-search-functionality-in-javascript/ 
+
+Sliders-carousel
+splide js vs flicking vs slick vs glide vs swiper 
+https://www.thecssagency.com/blog/5-best-sliders-to-enhance-your-webflow-website 
+
+
+
+Replicate the Site for portfolio->
+https://www.twine.net/
+https://www.lifewire.com/free-cloud-storage-1356638 
+ 
+
+SVG Animation
+https://pngtree.com/pay?b=120 
+https://www.svgator.com/ 
+
+
+—------------------
+Angular + React
+—------------------
+Comparison-> 
+https://www.browserstack.com/guide/angular-vs-react-vs-vue 
+
+
+https://webcache.googleusercontent.com/search?q=cache:https://javascript.plainenglish.io/use-chrome-devtools-like-a-senior-frontend-developer-97ad3ee47647&sca_esv=590835004&strip=1&vwsrc=0 
+
+
+
+
+https://www.freecodecamp.org/news/open-source-ai/
+https://www.freecodecamp.org/news/how-to-use-settimeout-in-react-using-hooks/
+
+
+
+
+
