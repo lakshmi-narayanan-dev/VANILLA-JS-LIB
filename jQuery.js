9c2d45bfeee98ade72eb7aa71789269ee6fa5742
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-23 19:01:17 +0530

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -4,3 +4,6 @@ Jquery is a powerful , lightweight & powerful JS Library.
 
 $ Symbol in Jquery  
 https://www.edureka.co/community/183307/what-is-the-meaning-of-symbol-in-jquery#:~:text=%24%20sign%20is%20just%20a%20valid,base%20object%20(or%20function) 
+
+Entire Jquery Learnings
+https://hackr.io/blog/how-to-learn-jquery#:~:text=JQuery%20is%20easy%2Dto%2Dlearn,it%20has%204%20basic%20uses 
