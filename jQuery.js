6444ba0ee3134cac1f51a6de8699d1aad05330de
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-25 03:31:28 +0530

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -1,6 +1,6 @@
 Jquery is a powerful , lightweight & powerful JS Library.
 
-
+https://releases.jquery.com/ - CDN
 
 $ Symbol in Jquery  
 https://www.edureka.co/community/183307/what-is-the-meaning-of-symbol-in-jquery#:~:text=%24%20sign%20is%20just%20a%20valid,base%20object%20(or%20function) 
