4a950f27cc5662e186627e436823644a097b2098
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-25 03:32:44 +0530

    Update Tabname-Focus-Blur.js

--- Tabname-Focus-Blur.js
+++ Tabname-Focus-Blur.js
@@ -1,10 +1,1 @@
-<!-- Tab name - Script For Tab Focus & Blur -->
-<script>
-    document.title = "GLF - Wishing Everyone a Happy Christmas!";
-    window.addEventListener("blur", () => {
-        document.title = "Please Have A Look!";
-    });
-    window.addEventListener("focus", () => {
-        document.title = "GLF - Wishing Everyone a Happy Christmas!";
-    });
-</script>
+
