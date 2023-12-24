d0040ece3be8f96b0af621dcd2ee2880fd7566a7
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-25 03:32:58 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -3,3 +3,15 @@
 
 Global Objects
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol 
+
+
+<!-- Tab name - Script For Tab Focus & Blur -->
+<script>
+    document.title = "GLF - Wishing Everyone a Happy Christmas!";
+    window.addEventListener("blur", () => {
+        document.title = "Please Have A Look!";
+    });
+    window.addEventListener("focus", () => {
+        document.title = "GLF - Wishing Everyone a Happy Christmas!";
+    });
+</script>
